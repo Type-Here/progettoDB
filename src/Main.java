ea5987c3f2ee4cc48065e1c82498cf0a1e02482e
type-here <94553428+Type-Here@users.noreply.github.com>
2023-12-25 13:41:05 +0100

    Added Main, .gitignore, environment

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.printf("Hello and welcome!");
+        MainGUI GUI = new MainGUI();
+    }
+}
