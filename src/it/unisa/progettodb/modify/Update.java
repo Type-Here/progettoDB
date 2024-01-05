514ba9323cb3956b6cc2c58ff5048888e1b2373d
type-here <94553428+Type-Here@users.noreply.github.com>
2024-01-06 00:44:50 +0100

    Update Class Draft

---
+++ src/it/unisa/progettodb/modify/Update.java
@@ -0,0 +1,42 @@
+package it.unisa.progettodb.modify;
+
+import it.unisa.progettodb.DBManagement;
+import it.unisa.progettodb.datacontrol.ContentPackage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class Update extends JOptionPane implements DataManipulation{
+
+    private final DBManagement managerDB;
+    private final String workingTable;
+    private final Component owner;
+    private JPanel mainDialogPanel;
+    private List<ContentPackage> contentPackageList;
+
+    public Update(Component owner, DBManagement managerDB, String workingTable) {
+        super(null, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_CANCEL_OPTION );
+        this.managerDB = managerDB;
+        this.workingTable = workingTable;
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean createDialog() {
+        return false;
+    }
+
+
+
+    /* THIS CHECK IS DATABSE SPECIFIC */
+
+    /**
+     * Check if Current Working Table Data can be Updated
+     * @return true if table if updatable, false if not
+     */
+    public boolean isUpdatable(){
+        return !this.workingTable.equalsIgnoreCase("dipendentiview");
+    }
+
+}
