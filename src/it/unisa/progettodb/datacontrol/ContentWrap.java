fed43ad30120c6285cec4d9751f8108a1dfeb91d
type-here <94553428+Type-Here@users.noreply.github.com>
2024-01-19 18:40:23 +0100

    Reorganization of Code.

--- src/it/unisa/progettodb/datacontrol/ContentWrap.java
+++ src/it/unisa/progettodb/datacontrol/ContentWrap.java
@@ -35,31 +35,6 @@ public class ContentWrap implements Cloneable{
 
 
     /**
-     * Method to parse ResultData to new ContentWrap Package (Standard Used in this Program).
-     * @param metaData List of ContentPackage. dataString is null but contains all needed metadata for each Column (JDBCType, ColumnName, Index,
-     *                 Precision, isNullable).
-     * @param dataSet ResultSet from a Query to parse data from
-     * @return HashMap with K:Integer=Num Row of insertion from DB. V:LIst of Strings containing All Row Data
-     * @throws SQLException if retrieving data from ResultSet fails
-     */
-    public static ContentWrap getContentWrap(List<ContentPackage> metaData, ResultSet dataSet) throws SQLException {
-       HashMap<Integer, List<String>> rows = new LinkedHashMap<>();
-        int rowLine = 1;
-        while(dataSet.next()){
-            List<String> row = new ArrayList<>();
-            for(int i = 1; i <= metaData.size(); i++){
-                if(metaData.get(i - 1).getType().equals(JDBCType.BIT) || metaData.get(i - 1).getType().equals(JDBCType.BOOLEAN)){
-                    row.add(dataSet.getString(i).equals("1") ? "Si" : "No");   //For Better View in Table
-                } else {
-                    row.add(dataSet.getString(i));
-                }
-            }
-        rows.put(rowLine++, row);
-        }
-        return new ContentWrap(metaData, rows);
-    }
-
-    /**
      * Retrieve Automatically all MetaData for each Row. <br />
      * Method to parse ResultData to new ContentWrap Package (Standard Used in this Program).
      * @param dataSet ResultSet from a Query to parse Data and MetaData from
@@ -82,6 +57,35 @@ public class ContentWrap implements Cloneable{
             return getContentWrap(metaData, dataSet);
         }
     }
+
+    /* ---------------------------- PRIVATE ------------------------------- */
+
+    /**
+     * Method to parse ResultData to new ContentWrap Package (Standard Used in this Program).
+     * @param metaData List of ContentPackage. dataString is null but contains all needed metadata for each Column (JDBCType, ColumnName, Index,
+     *                 Precision, isNullable).
+     * @param dataSet ResultSet from a Query to parse data from
+     * @return HashMap with K:Integer=Num Row of insertion from DB. V:LIst of Strings containing All Row Data
+     * @throws SQLException if retrieving data from ResultSet fails
+     */
+    private static ContentWrap getContentWrap(List<ContentPackage> metaData, ResultSet dataSet) throws SQLException {
+        HashMap<Integer, List<String>> rows = new LinkedHashMap<>();
+        int rowLine = 1;
+        while(dataSet.next()){
+            List<String> row = new ArrayList<>();
+            for(int i = 1; i <= metaData.size(); i++){
+                if(metaData.get(i - 1).getType().equals(JDBCType.BIT) || metaData.get(i - 1).getType().equals(JDBCType.BOOLEAN)){
+                    row.add(dataSet.getString(i).equals("1") ? "Si" : "No");   //For Better View in Table
+                } else {
+                    row.add(dataSet.getString(i));
+                }
+            }
+            rows.put(rowLine++, row);
+        }
+        return new ContentWrap(metaData, rows);
+    }
+
+    /*-------------------------------- OVERRIDES ----------------------------- */
 
     /**
      * Overrides Object.clone(). <br />
